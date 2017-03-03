c3a794d07803b4091c8f3a9b6cff586472a5913c
Eunbi Go <eunbigo91@gmail.com>
2017-03-02 18:26:08 -0800

    Add: Date of projects

--- js/projects.js
+++ js/projects.js
@@ -7,6 +7,7 @@ $(function(){
             {
                 "title" : "Movie Trailer",
                 "pic" : "img/project1.png",
+                "date" : "Jul. 2016",
                 "descriptions" : "Server-side code written in Python to store a list of my favorite movies. This data is then served to a web page using HTML and CSS allowing visitors to review these movies and watch the trailers.",
                 "url" : "",
                 "githubUrl" : "https://github.com/eunbigo91/project1-movie-trailer"
@@ -14,6 +15,7 @@ $(function(){
             {
                 "title" : "Build Portfolio",
                 "pic" : "img/project2.png",
+                "date" : "Aug. 2016",
                 "descriptions" : "Developed a personal portfolio page using HTML, CSS, and the Bootstrap framework. The page is fully responsive and works on mobile, tablet, and desktop browsers.",
                 "url" : "",
                 "githubUrl" : "https://github.com/eunbigo91/project2-build-portfolio"
@@ -21,6 +23,7 @@ $(function(){
             {
                 "title" : "Multi User Blog",
                 "pic" : "img/project3.png",
+                "date" : "Oct. 2016",
                 "descriptions" : "Built a multi-user blog, hosted on Google App Engine, with comments and login functionality. Developed authentication functionality using hmac module and saved users’ password securely using hashlib. ",
                 "url" : "",
                 "githubUrl" : "https://github.com/eunbigo91/project3-multi-user-blog"
@@ -28,6 +31,7 @@ $(function(){
             {
                 "title" : "Item Catalog",
                 "pic" : "img/project5.png",
+                "date" : "Dec. 2016",
                 "descriptions" : "Developed a content management system using the Flask framework in Python. Authentication is provided via OAuth and all data is stored within a PostgreSQL database.",
                 "url" : "",
                 "githubUrl" : "https://github.com/eunbigo91/project5-item-catalog"
@@ -35,6 +39,7 @@ $(function(){
             {
                 "title" : "Neighborhood Map",
                 "pic" : "img/project6.png",
+                "date" : "Jan. 2017",
                 "descriptions" : "A single-page web application, built using the Knockout framework, that displays a Google Map of an area and various points of interest. Users can search all included landmarks and, when selected, additional information about a landmark is presented from the FourSquare and Wikipedia APIs.",
                 "url" : "",
                 "githubUrl" : "https://github.com/eunbigo91/project6-neighborhood-map"
@@ -42,6 +47,7 @@ $(function(){
             {
                 "title" : "Personal Blog Using Wordpress",
                 "pic" : "img/blog.png",
+                "date" : "Dec. 2016",
                 "descriptions" : "Used Wordpress and Wordpress basic theme to build my personal blog.",
                 "url" : "",
                 "githubUrl" : "https://github.com/eunbigo91/myWordPress"
@@ -93,6 +99,7 @@ $(function(){
             var project = octopus.getProjects()[selected];
             html += '<h3 class="detail-title text-center">'+project.title+'</h3>';
             html += '<picture> <img class="detail-image" src="'+project.pic+'" alt="project screen shot"></picture>';
+            html += '<p class="detail-text">'+project.date+'</p>';
             html += '<p class="detail-text">'+project.descriptions+'</p>';
             html += '<div class="text-center detail-icons">';
             html += '<i class="fa fa-play-circle" style="font-size:30px" aria-hidden="true"></i>';
